8fa8c9ba8678ff3202c491e9995156aea4d41141
Abdul Baari Davids <abdulbaaridavids04@gmail.com>
2026-02-25 22:45:08 +0200

    Initial Commit

---
+++ thoughtcomplete/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thoughtcomplete
+//
+//  Created by Abdul Baari Davids on 2026/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thoughtcomplete/thoughtcompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtcompleteApp.swift
+//  thoughtcomplete
+//
+//  Created by Abdul Baari Davids on 2026/02/25.
+//
+
+import SwiftUI
+
+@main
+struct thoughtcompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
